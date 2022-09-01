87325cd6fb9d25c8f4a25dabed7ab3edab4675e7
Aman <akapur07@gmail.com>
2022-09-01 20:09:44 +0530

    Patterns

--- com/aman/patterns/Pattern22.java
+++ com/aman/patterns/Pattern22.java
@@ -1,11 +1,22 @@
 package com.aman.patterns;
 
+
+//These question are hit and try
+// 4444444
+// 4333334
+// 4322234
+// 4321234
+// 4322234
+// 4333334
+// 4444444
 public class Pattern22 {
     public static void main(String[] args) {
         int n=4;
         //upper
         for(int i=0;i<n;i++){
-            //left
+            //left- left we want n-i 
+            //4444 => 4-0 (i=0, j=0)
+            //4333 => 4-i (i=1, j=increses) but only if j becomes greter then i
             for(int j=0;j<n;j++){
                 if(j>=i){
                     System.out.print(n-i);
@@ -15,6 +26,7 @@ public class Pattern22 {
             }
 
             //right
+            //right vice versa
             for(int j=n-2;j>=0;j--){
                 if(j>=i){
                     System.out.print(n-i);
